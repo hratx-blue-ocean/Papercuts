826a5d15b98c1df30ddba5d6ed59b404f4dae3b4
Matthew Budiman <matthewbudiman@outlook.com>
2021-02-12 16:29:47 -0600

    Added email

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
-const express = require("express");
-
+const expressStaticGzip = require('express-static-gzip');
+const express = require('express');
 const app = express();
 
-const expressStaticGzip = require("express-static-gzip");
+const port = process.env.PORT || 3000;
 
-const port = 3000;
+// Body parser
+app.use(express.json());
+
+// Routes
+app.use('/bookclub', require('./routes/BookclubRoute'));
+app.use('/user', require('./routes/UserRoute'));
+app.use('/book', require('./routes/BookRoute'));
 
 app.use(
-  "/",
-  expressStaticGzip("client/dist", {
+  '/',
+  expressStaticGzip('client/dist', {
     enableBrotli: true,
-    orderPreference: ["br"],
+    orderPreference: ['br'],
     setHeaders: function (res, path) {
-      res.setHeader("Cache-Control", "public, max-age=31536000");
+      res.setHeader('Cache-Control', 'public, max-age=31536000');
     },
   })
 );
