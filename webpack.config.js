323031ffef23eb29c92fce65b6237b57581c27ac
Matthew Budiman <matthewbudiman@outlook.com>
2021-02-11 19:06:54 -0600

    Clean template

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', __dirname + '/client/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css?/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
