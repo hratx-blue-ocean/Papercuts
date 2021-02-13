506b358aaf2d90483a55d9599f4fb170c4e6316f
Daniel He <danielhe.mlp@gmail.com>
2021-02-13 11:43:54 -0600

    added test image for development

--- client/components/bookClubPage/ClubBanner.jsx
+++ client/components/bookClubPage/ClubBanner.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import {Container, Row, Col, Button, Image} from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
+import test from "./test.jpg";
 
-export default function ClubBanner({name, description, members}) {
+export default function ClubBanner({ name, description, members }) {
   const numMembers = members.length;
 
   return (
     <Container>
       <Row>
         <Col>
-          <Image src="./test.jpg" rounded fluid />
+          <Image src={test} rounded fluid />
         </Col>
         <Col>
           <Row>
-            <h1>{name} <Button variant="outline-info" disabled >{`${numMembers} members`}</Button></h1>
+            <h1>
+              {name}{" "}
+              <Button
+                variant="outline-info"
+                disabled
+              >{`${numMembers} members`}</Button>
+            </h1>
           </Row>
           <Row>
             <p>{description}</p>
@@ -20,5 +27,5 @@ export default function ClubBanner({name, description, members}) {
         </Col>
       </Row>
     </Container>
-  )
+  );
 }
