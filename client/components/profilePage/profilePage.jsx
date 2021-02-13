5b39727849faf1d5ea4ebf0cc0f46d158cacc776
scbrazil <seancbrazil@gmail.com>
2021-02-12 19:26:42 -0600

    Initialized Profile component; added to App.jsx

---
+++ client/components/profilePage/profilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div>
+      This is the single more important component of the entire project. Fight me.
+    </div>
+  )
+}
