af1df37188548f01e7d2bee3ad8e79cd20754d58
Chris Thackrey <c.r.thackrey@gmail.com>
2021-02-15 16:08:40 -0600

    minor tweaks

--- client/App.jsx
+++ client/App.jsx
@@ -27,13 +27,6 @@ export const App = () => {
       <Header user={user} />
       <main className='py-3'>
         <Container>
-          {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
           <Switch>
             <Route exact path='/'>
               {/* Cayla, put in your component here when ready */}
