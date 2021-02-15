13aa6b944ce6085e7dbba1f1807ece5480507eb2
Chris Thackrey <c.r.thackrey@gmail.com>
2021-02-15 00:39:32 -0600

    update package.json

---
+++ client/components/bookClubPage/bookClubs.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {} from 'react-bootstrap';
+import { AppContext } from '../../context/context.jsx';
+
+import Schedule from './Schedule.jsx';
+import Comments from './Comments.jsx';
+import Questionnaire from './Questionnaire.jsx';
+import exampleClubs from './exampleData.js';
+import ClubBanner from './ClubBanner.jsx';
+
+{
+  /* <Link to={`/bookclub/${bookclub.id}`}></Link>; */
+}
