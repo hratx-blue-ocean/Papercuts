e53807a152eb5fe0dcfec951b9d58681eadaaae9
CheddarChzKeys <jerrick.ravelo@gmail.com>
2021-02-15 12:58:47 -0800

    My Library and Search Bar Integration

--- client/components/profilePage/myLibrary.jsx
+++ client/components/profilePage/myLibrary.jsx
@@ -1,40 +1,40 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React, {useState} from 'react';
+import axios from 'axios';
 
 
 export default function myLibrary() {
   const books = [
     {
       volumeInfo: {
-        title: "The Cat in The Hat",
-        authors: "Dr. Seuss",
+        title: 'The Cat in The Hat',
+        authors: 'Dr. Seuss',
         industryIdentifiers:[{identifier:7891036757892}]
       },
-      description: "The Cat in The Hat jumps back!",
-      isbn: "9780375850967",
+      description: 'The Cat in The Hat jumps back!',
+      isbn: '9780375850967',
       image:
-        "http://books.google.com/books/content?id=FtVIabJkEUcC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        'http://books.google.com/books/content?id=FtVIabJkEUcC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
       price: 13.99,
-      category: "Juvenile Fiction",
+      category: 'Juvenile Fiction',
       rating: 3,
       ratingCount: 11,
-      datePurchased: "11/30/2021",
+      datePurchased: '11/30/2021',
     },
     {
       volumeInfo: {
-        title: "The Grinch",
-        authors: "Dr. Seuss",
+        title: 'The Grinch',
+        authors: 'Dr. Seuss',
         industryIdentifiers:[{identifier:7891036457892}]
         },
-      description: "Youre a mean one, Mr. Grinch!",
-      isbn: "5760375843767",
+      description: 'Youre a mean one, Mr. Grinch!',
+      isbn: '5760375843767',
       image:
-        "http://books.google.com/books/content?id=FtVIabJkEUcC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        'http://books.google.com/books/content?id=FtVIabJkEUcC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
       price: 19.99,
-      category: "Juvenile Fiction",
+      category: 'Juvenile Fiction',
       rating: 5,
       ratingCount: 7,
-      datePurchased: "02/15/2021",
+      datePurchased: '02/15/2021',
     },
   ];
 
