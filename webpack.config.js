394f05043d199d2bc9c34821e5a0e6add8fbe686
Chris Thackrey <c.r.thackrey@gmail.com>
2021-02-15 06:39:42 -0600

    update webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const CompressionPlugin = require("compression-webpack-plugin");
-const zlib = require("zlib");
+const CompressionPlugin = require('compression-webpack-plugin');
+const zlib = require('zlib');
 
 module.exports = {
-  entry: ["@babel/polyfill", `${__dirname}/client/index.js`],
+  entry: ['@babel/polyfill', `${__dirname}/client/index.js`],
   plugins: [
     new CompressionPlugin({
-      filename: "[path][base].br",
-      algorithm: "brotliCompress",
+      filename: '[path][base].br',
+      algorithm: 'brotliCompress',
       test: /\.(js|css|html|svg)$/,
       compressionOptions: {
         params: {
@@ -24,31 +24,31 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-            plugins: [["@babel/plugin-proposal-class-properties"]],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: [['@babel/plugin-proposal-class-properties']],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
+        test: /.(jpe?g|png|gif)$/,
+        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+      },
+      {
+        test: /.(eot|svg|ttf|woff|woff2)$/,
+        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
       },
     ],
   },
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: `${__dirname}/client/dist`,
   },
-  mode: "development",
+  mode: 'development',
   // mode: "production",
 };
