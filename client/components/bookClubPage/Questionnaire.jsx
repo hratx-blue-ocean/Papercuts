d4bd2b6b5c633f2c0e224a84c96fa83145ea1a07
Daniel He <danielhe.mlp@gmail.com>
2021-02-16 16:23:37 -0600

    questionnaire now uses static questions. Submission works. Alert will pop up upon successful submission

--- client/components/bookClubPage/Questionnaire.jsx
+++ client/components/bookClubPage/Questionnaire.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { AppContext } from '../../context/context.jsx';
 
-export default function Questionnaire({ questionnaire }) {
+const questionnaire = [
+  'From 1 (Worst) to 10 (Best), how would you rate your experience in our club?',
+  'Would you be willing to recommend our book club to friends and family?',
+  'What is your favorite book club event you have attended so far?',
+  'What was your favorite part of the book?',
+  'If you could ask the author one thing, what would it be?',
+  "Anything else you'd like to let us know about your experience in our book club",
+];
+
+export default function Questionnaire({}) {
+  const { selectedClubData } = useContext(AppContext);
+  const [formData, setFormData] = useState({});
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmitQuestionnaire = (e) => {
+    axios
+      .post(`/bookclub/questionnaire/${selectedClubData._id}`, formData)
+      .then(() => setShowAlert(true));
+  };
+
   return (
-    <Form>
-      {questionnaire.map((question, idx) => (
-        <Form.Group key={idx} controlId={`question-${idx}`}>
-          <Form.Label>{question.question}</Form.Label>
-          <Form.Control type={question.type} />
-        </Form.Group>
-      ))}
-      <Button variant='primary' type='submit'>
-        Submit
-      </Button>
-    </Form>
+    <>
+      {showAlert && (
+        <Alert variant='success'>
+          Successfully submitted questionnaire. Thanks for your feedback!
+        </Alert>
+      )}
+      <Form>
+        {questionnaire.map((question, idx) => (
+          <Form.Group key={idx + 1} controlId={`question-${idx + 1}`}>
+            <Form.Label>{question}</Form.Label>
+            <Form.Control
+              required
+              type='text'
+              onChange={(e) => setFormData({ ...formData, [`answer_${idx + 1}`]: e.target.value })}
+            />
+          </Form.Group>
+        ))}
+        <Button variant='primary' onClick={handleSubmitQuestionnaire}>
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 }
