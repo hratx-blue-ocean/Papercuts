78cf69e005664b9f383ead346ea0453ee88d2fe1
Daniel He <danielhe.mlp@gmail.com>
2021-02-16 16:20:47 -0600

    Added working join/leave button ClubBanner

--- client/components/bookClubPage/ClubBanner.jsx
+++ client/components/bookClubPage/ClubBanner.jsx
@@ -1,25 +1,61 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
 import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+import { AuthContext } from '../../context/authContext.jsx';
+import { AppContext } from '../../context/context.jsx';
 import test from './test.jpg';
 
-export default function ClubBanner({ name, description, members }) {
-  let numMembers = 0;
-  members ? (numMembers = members.length) : {};
+export default function ClubBanner({}) {
+  const user = useContext(AuthContext);
+  const { selectedClubData } = useContext(AppContext);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    setData(selectedClubData);
+  }, [selectedClubData]);
+
+  const handleJoinClub = () => {
+    axios
+      .post(`/bookclub/join/${selectedClubData._id}`, { userId: user._id })
+      .then(() => setData({ ...data, members: [...data.members, user._id] }));
+  };
+
+  const handleLeaveClub = () => {
+    axios
+      .post(`/bookclub/join/${selectedClubData._id}`, { userId: user._id })
+      .then(() =>
+        setData({ ...data, members: data.members.filter((member) => member !== user._id) })
+      );
+  };
 
   return (
     <Container>
       <Row>
         <Col>
-          <Image src={test} rounded fluid />
+          <Image src={data.thumbnail || ''} rounded fluid />
         </Col>
         <Col>
           <Row>
             <h1>
-              {name} <Button variant='outline-info' disabled>{`${numMembers} members`}</Button>
+              {data.name || ''}{' '}
+              <Button variant='outline-info' disabled>{`${
+                data.members ? data.members.length : 0
+              } members`}</Button>
+              {user &&
+                data.members &&
+                (data.members.includes(user._id) ? (
+                  <Button variant='secondary' onClick={handleLeaveClub}>
+                    Leave
+                  </Button>
+                ) : (
+                  <Button variant='primary' onClick={handleJoinClub}>
+                    Join
+                  </Button>
+                ))}
             </h1>
           </Row>
           <Row>
-            <p>{description}</p>
+            <p>{data.description || ''}</p>
           </Row>
         </Col>
       </Row>
