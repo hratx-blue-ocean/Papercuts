1cf4a4423233646d96cfda462e85528c56c07e67
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-02-16 12:18:54 -0600

    wrote the post request for book

--- server/routes/BookRoute.js
+++ server/routes/BookRoute.js
@@ -1,14 +1,22 @@
 const router = require('express').Router();
-
+const Book = require('../models/books');
 // @desc    Display 7 trending books
 // @route   GET /book/trending
 // @access  Public
 router.get('/trending', (req, res) => {});
 
-// @desc    Display 7 book according to the genre
-// @route   GET /book/genre/:genre
-// @access  Public
-router.get('/genre/:genre', (req, res) => {});
+// @desc    Post a book to save in the database
+// @route   POST /book
+router.post('/', async (req, res) => {
+  try {
+    const newBook = new Book(req.body);
+    const savedDoc = await newBook.save();
+
+    res.status(201).send(savedDoc);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 // @desc    Get book of the month
 // @route   GET /book/ofthemonth
