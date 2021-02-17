ff72ae6a515bf04946aeb889c4372940c2bebae3
Matthew Budiman <matthewbudiman@outlook.com>
2021-02-17 10:58:12 -0600

    Made footer smaller for now... and center it

--- client/components/global/NewFooter.js
+++ client/components/global/NewFooter.js
@@ -4,7 +4,7 @@ import { Navbar, Container, Row, Col } from 'react-bootstrap';
 export const NewFooter = () => {
   return (
     <Navbar id='footer' className='footer bg-light'>
-      <Container className='text-center vw-100'>
+      <Container className='d-flex justify-content-center'>
         Copyright &copy; Papercuts Books 2020-2021
       </Container>
     </Navbar>
