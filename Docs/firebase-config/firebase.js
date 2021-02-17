4a3cca109063081f7fdbc3ab64ac6a3b5689b09f
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-02-16 19:01:45 -0600

    sorry, thought i changed them

--- Docs/firebase-config/firebase.js
+++ Docs/firebase-config/firebase.js
@@ -1,19 +1,5 @@
 import Firebase from 'firebase/app';
 import 'firebase/storage';
-<<<<<<< HEAD
-
-var firebaseConfig = {
-  apiKey: 'AIzaSyAzU8hVZVKslQ9y7RVS9c5hgI5hVktDXQQ',
-  authDomain: 'papercuts-8b86c.firebaseapp.com',
-  projectId: 'papercuts-8b86c',
-  storageBucket: 'papercuts-8b86c.appspot.com',
-  messagingSenderId: '554966896828',
-  appId: '1:554966896828:web:cff09af0782797843200b9',
-};
-
-const app = Firebase.initializeApp(firebaseConfig);
-
-=======
 var firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -23,5 +9,4 @@ var firebaseConfig = {
   appId: process.env.APP_ID,
 };
 const app = Firebase.initializeApp(firebaseConfig);
->>>>>>> 1c72f4d8f66f7cb549b31e7372f77095dac756da
 export const firebaseStorage = app.storage();
