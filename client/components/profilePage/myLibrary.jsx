a6da14d5996dff3a8850ab0fd0af32a660960e02
Jason Fleming <jfleming9357@gmail.com>
2021-02-18 16:33:13 -0500

    stuff

--- client/components/profilePage/myLibrary.jsx
+++ client/components/profilePage/myLibrary.jsx
@@ -1,88 +1,89 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Button, ListGroup, Container } from 'react-bootstrap';
 import BookDetail from '../global/BookDetail.jsx';
 import RecommendedBooks from './recommendedBooks.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function myLibrary() {
-  const books =
-  [{
-      title: 'Green Eggs & Ham',
-      authors: ['Dr. Seuss'],
-      isbn: 7891036757892,
-      description: 'I do not like my green eggs & ham, Sam I am',
-      isbn: '9780375850967',
-      image:
-        'http://books.google.com/books/content?id=h7w4DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-      price: '$13.99',
-      category: 'Juvenile Fiction',
-      rating: 3,
-      ratingCount: 11,
-      datePurchased: '11/30/2021',
-    },
-    {
-      title: 'One Fish, Two Fish',
-      authors: ['Dr. Seuss'],
-      isbn: 7891036457892,
-      description: 'One Fish, Two Fish, Red Fish, Blue Fish',
-      isbn: '5760375843767',
-      image:
-        'http://books.google.com/books/content?id=067xAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-      price: '$19.99',
-      category: 'Juvenile Fiction',
-      rating: 5,
-      ratingCount: 7,
-      datePurchased: '02/15/2021',
-    },
-  ];
-
-  const [booksOwned, setBooksOwned] = useState(books);
+const myLibrary = async (props) => {
+  const [booksOwned, setBooksOwned] = useState([]);
   const [searchInput, setSearchInput] = useState('');
   const [clickedBook, setClickedBook] = useState();
 
+  useEffect(() => {
+    try {
+      axios
+        .get('user/book', { userId: props.user._id })
+        .then((userBooks) => setBooksOwned(userBooks.data));
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   let searchBooks = function (e) {
     e.preventDefault();
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${searchInput}&maxResults=25`)
-    .then((results)=> {
-      let searchResults = results.data.items.map((book) => {
-        let bookInfo = {};
-        bookInfo.title= book.volumeInfo.title,
-        bookInfo.authors= book.volumeInfo.authors,
-        bookInfo.isbn= book.volumeInfo.industryIdentifiers[0].identifier;
-        bookInfo.description = book.volumeInfo.description;
-        bookInfo.image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/sJ3CT4V.gif';
-        bookInfo.price = book.saleInfo.listPrice ? `$${book.saleInfo.listPrice.amount}` : 'Not Available'
-        return bookInfo;
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=inauthor:${searchInput}&maxResults=25`
+      )
+      .then((results) => {
+        let searchResults = results.data.items.map((book) => {
+          let bookInfo = {};
+          (bookInfo.title = book.volumeInfo.title),
+            (bookInfo.authors = book.volumeInfo.authors),
+            (bookInfo.isbn = book.volumeInfo.industryIdentifiers[0].identifier);
+          bookInfo.description = book.volumeInfo.description;
+          bookInfo.image = book.volumeInfo.imageLinks
+            ? book.volumeInfo.imageLinks.thumbnail
+            : 'https://i.imgur.com/sJ3CT4V.gif';
+          bookInfo.price = book.saleInfo.listPrice
+            ? `$${book.saleInfo.listPrice.amount}`
+            : 'Not Available';
+          return bookInfo;
+        });
+        setBooksOwned(searchResults);
       });
-      setBooksOwned(searchResults);
-    })
-  }
+  };
 
   const [show, setShow] = useState(false);
   return (
     <div id='myLib'>
       <div>My Library</div>
-      <form onSubmit = {searchBooks.bind(this)}>
-        <input type = 'text' placeholder = 'Search books by author' onChange = {(e)=> setSearchInput(e.target.value)}/>
-        <input type = 'submit'/>
+      <form onSubmit={searchBooks.bind(this)}>
+        <input
+          type='text'
+          placeholder='Search books by author'
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <input type='submit' />
       </form>
       <div id='libraryBody'>
-      {booksOwned.map((book) => {
-        return(
-          <div className='bookBody' key = {book.isbn}>
-            <img className='bookImage' variant='primary' onClick={() => {
-              setClickedBook(book);
-              setShow(true);
-              }
-              } src={book.image} >
-            </img>
-            <BookDetail handleClose={() => { setShow(false) }} show={show} book = {clickedBook}/>
-          </div>
-        )
-      })}
+        {booksOwned.map((book) => {
+          return (
+            <div className='bookBody' key={book.isbn}>
+              <img
+                className='bookImage'
+                variant='primary'
+                onClick={() => {
+                  setClickedBook(book);
+                  setShow(true);
+                }}
+                src={book.image}
+              ></img>
+              <BookDetail
+                handleClose={() => {
+                  setShow(false);
+                }}
+                show={show}
+                book={clickedBook}
+              />
+            </div>
+          );
+        })}
       </div>
-      <RecommendedBooks/>
+      <RecommendedBooks />
     </div>
-  )
+  );
 };
+
+export default myLibrary;
