26d66e5ad37783b6a8f7fd413fdf893eee94b6bc
CheddarChzKeys <jerrick.ravelo@gmail.com>
2021-02-18 07:54:48 -0800

    Merge w/ user Add Friends, Dynamic recommended books & book clubs

--- client/components/profilePage/allFriendsModal.jsx
+++ client/components/profilePage/allFriendsModal.jsx
@@ -2,49 +2,37 @@ import React, { useState, useEffect } from 'react';
 import { Modal, Image, Button, Table, Badge, Spinner, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
-export default function AllUsers({
+export default function AllFriends({
+  friendsList,
   handleClose,
   show,
-  user,
 }) {
 
-  const addFriend = (newFriendId) => {
-    axios.post('/user/friend', {userId: user._id, friendId: newFriendId})
-  }
-  const [allUsers, setAllUsers] = useState([]);
 
-  useEffect(async () => {
-    let usersList = await axios.get(`http://localhost:3000/user/all`)
-    setAllUsers(usersList.data);
-  }, []);
 
-  return allUsers === undefined ? (
+  return friendsList === undefined ? (
     <Spinner animation='border' role='status'>
       <span className='sr-only'>Loading...</span>
     </Spinner>
   ) : (
     <Modal size='lg' show={show} onHide={handleClose} backdrop='static' keyboard={false}>
       <Modal.Header closeButton>
-        <Modal.Title>All Users</Modal.Title>
+        <Modal.Title>All Friends</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Row>
           <Col>
-            {allUsers.map((user) => {
+            {friendsList.map((friend) => {
               return (
-              <Row className = 'm-3' key={user._id}>
+              <Row className = 'm-3' key={friend._id}>
                 <div className='thumbBody mx-3'>
-                  <img className='thumbImage' src={user.photoUrl}/>
+                  <img className='thumbImage' src={friend.photoUrl}/>
                 </div>
-                <span>{user.username}</span>
-                <Button className='mx-3' variant='secondary' onClick={() => {addFriend(user._id)}}>
-                  Add Friend
-                </Button>
+                <span>{friend.username}</span>
               </Row>)
             })}
           </Col>
         </Row>
-
       </Modal.Body>
       <Modal.Footer>
         <Button variant='secondary' onClick={handleClose}>
--- client/components/profilePage/myProfile.jsx
+++ client/components/profilePage/myProfile.jsx
@@ -1,20 +1,29 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import axios from 'axios';
 import image1 from './avatar1.png';
 import FriendRecommendations from './friendRecommendations.jsx';
 import UserBookClubs from './userBookClubs.jsx';
 import AllUsersModal from './allUsersModal.jsx';
+import AllFriendsModal from './allFriendsModal.jsx';
 import { Button, ListGroup, Container } from 'react-bootstrap';
 import AuthContext from '../../context/authContext.jsx';
 
-export default function myFriends({user}) {
+export default function myFriends({user = {friends:[]}}) {
   const currentUser = {
     username: 'bookwormboy85',
     friends: [{username: 'Josh'},{username: 'Maddy'}, {username: 'Spencer'},{username: 'Abe'},{username: 'Jeffrey'}],
     subscription: 'platinum subscription'
   }
-  const [show, setShow] = useState(false);
-  console.log(user);
+  const [allUsersShow, setAllUsersShow] = useState(false);
+  const [allFriendsShow, setAllFriendsShow] = useState(false);
+  const [friendsList, setFriendsList] = useState([]);
+
+  useEffect(()=>{
+    axios.post('/user/friends', {userId : user._id})
+    .then ((friends) => {
+      setFriendsList(friends.data);
+    })
+  },[])
 
   return(
     <div id='myProfile'>
@@ -24,26 +33,25 @@ export default function myFriends({user}) {
       </div>
       <div id='friendsLinks'>
         <a>Friends</a>
-        <a variant='primary' onClick={() => { setShow(true) }}>Find Friends</a>
-        <AllUsersModal handleClose={() => { setShow(false) }} show={show} />
-        <a>View All Friends</a>
+        <a variant='primary' onClick={() => { setAllUsersShow(true) }}>Find Friends</a>
+        <AllUsersModal handleClose={() => { setAllUsersShow(false) }} show={allUsersShow} user={user} />
+        <a variant='primary' onClick={() => { setAllFriendsShow(true) }}>View All Friends</a>
+        <AllFriendsModal handleClose={() => { setAllFriendsShow(false) }} show={allFriendsShow} friendsList={friendsList}/>
       </div>
       <div id='myFriends'>
-        {
-        user.friends.map((friend) => {
-
-          return (
-            <div className = 'friendsDiv' key = {friend.username}>
-              <div><img src={image1}></img></div>
-              <div>{friend.username}</div>
-            </div>
-          )
-        })
+        {friendsList.length > 0 &&
+          friendsList.map((friend) => {
+            return(
+              <div className = 'friendsDiv' key = {friend.username}>
+                <div className = 'friendsBody'><img className='friendsImage' src={friend.photoUrl}></img></div>
+                <div>{friend.username}</div>
+              </div>
+            )
+          })
         }
       </div>
-      <FriendRecommendations user = {user}/>
+      <FriendRecommendations friendsList = {friendsList}/>
       <UserBookClubs user = {user}/>
-
     </div>
   )
 
