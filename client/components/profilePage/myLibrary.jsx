bda6463d92aedd74a37ff0c820fe0059b2b7044f
Jason Fleming <jfleming9357@gmail.com>
2021-02-18 16:34:17 -0500

    removed some changes

--- client/components/profilePage/myLibrary.jsx
+++ client/components/profilePage/myLibrary.jsx
@@ -5,20 +5,41 @@ import BookDetail from '../global/BookDetail.jsx';
 import RecommendedBooks from './recommendedBooks.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const myLibrary = async (props) => {
-  const [booksOwned, setBooksOwned] = useState([]);
+export default function myLibrary() {
+  const books = [
+    {
+      title: 'Green Eggs & Ham',
+      authors: ['Dr. Seuss'],
+      isbn: 7891036757892,
+      description: 'I do not like my green eggs & ham, Sam I am',
+      isbn: '9780375850967',
+      image:
+        'http://books.google.com/books/content?id=h7w4DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+      price: '$13.99',
+      category: 'Juvenile Fiction',
+      rating: 3,
+      ratingCount: 11,
+      datePurchased: '11/30/2021'
+    },
+    {
+      title: 'One Fish, Two Fish',
+      authors: ['Dr. Seuss'],
+      isbn: 7891036457892,
+      description: 'One Fish, Two Fish, Red Fish, Blue Fish',
+      isbn: '5760375843767',
+      image:
+        'http://books.google.com/books/content?id=067xAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+      price: '$19.99',
+      category: 'Juvenile Fiction',
+      rating: 5,
+      ratingCount: 7,
+      datePurchased: '02/15/2021'
+    }
+  ];
+
+  const [booksOwned, setBooksOwned] = useState(books);
   const [searchInput, setSearchInput] = useState('');
   const [clickedBook, setClickedBook] = useState();
-
-  useEffect(() => {
-    try {
-      axios
-        .get('user/book', { userId: props.user._id })
-        .then((userBooks) => setBooksOwned(userBooks.data));
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
 
   let searchBooks = function (e) {
     e.preventDefault();
@@ -84,6 +105,4 @@ const myLibrary = async (props) => {
       <RecommendedBooks />
     </div>
   );
-};
-
-export default myLibrary;
+}
