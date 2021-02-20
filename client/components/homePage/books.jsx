8bd2f156484503427d74e76384b6a8a7e5ac850e
Daniel He <danielhe.mlp@gmail.com>
2021-02-20 11:23:20 -0600

    unused files removed

--- client/components/homePage/books.jsx
+++ client/components/homePage/books.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import BookCarousel from './booksCarousel.jsx';
-import BookModal from './bookModal.jsx';
 import BookDetail from '../global/BookDetail.jsx';
 
 const Books = () => {
